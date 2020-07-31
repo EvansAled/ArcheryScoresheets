c6a7564c34e86907944b7eb39b2b460b0e367c09
Aled Evans <AledEvans0@protonmail.com>
2020-07-31 23:47:29 +0100

    View model for current scoresheets with entries

---
+++ ScoresheetsLibrary/ViewModels/ScoresSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ScoresheetsLibrary.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScoresheetsLibrary.ViewModels
+{
+    public class ScoresSession
+    {
+        public List<Entry> CurrentEntries { get; private set; }
+        public Config CurrentConfig { get; private set; }
+
+        public ScoresSession()
+        {
+            CurrentEntries = new List<Entry>();
+            CurrentConfig = new Config();
+            NewEntry("testOne");
+        }
+
+        public void NewEntry(string entryName)
+        {
+            CurrentEntries.Add(new Entry(entryName,
+                                         CurrentConfig));
+        }
+    }
+}
